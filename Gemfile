ea780248ce8afc01241242137abdc0e8e200be37
Denis Treskunov <denis.treskunov@gmail.com>
2017-10-25 11:22:19 -0700

    Enable fetching of EXIF data from originals
    * EXIF will be cached by URL (in caches/gphoto_exif.yml)
    * Consolidate secrets (deploy keys, OAuth keys, etc) in
      secrets.tar.gz.enc (encrypted via `travis encrypt-file`)
    * Add caches/ and build/ to Travis cache

--- Gemfile
+++ Gemfile
@@ -11,3 +11,5 @@ gem 'deep_merge'
 
 gem 'bigdecimal' # dependency of 'liquid' - not sure why it needs to be called out here
 gem 'json' # dependency of 'jekyll' - not sure why it needs to be called out here
+
+gem 'exif'
--- _plugins/gphoto.rb
+++ _plugins/gphoto.rb
@@ -3,6 +3,9 @@ require 'googleauth/stores/file_token_store'
 require 'picasa'
 require 'shellwords'
 require 'deep_merge'
+require 'exif'
+require 'open-uri'
+require 'yaml'
 
 # Google responds with incorrect encoding if a User-Agent containing "gzip" is used
 # BAD: ruby-gem-picasa-v0.9.1 (gzip)
@@ -58,8 +61,9 @@ module Jekyll
   module GPhoto
     module Base
       DEFAULT_CONFIG = {
-        'client_secrets_json' => '.gphoto/client_secrets.json',
-        'tokens_yml' => '.gphoto/tokens.yml'
+        'client_secrets_json' => 'secrets/gphoto_client_secrets.json',
+        'tokens_yml' => 'secrets/gphoto_tokens.yml',
+        'exif_cache_yml' => 'caches/gphoto_exif.yml'
       }
 
       ERR_EMAIL = "Specify `gphoto.email` in _config.yml"
@@ -70,50 +74,103 @@ key `gphoto.client_secrets_json`.
 
 https://console.developers.google.com/apis/credentials
 EOS
-      
-      def get_client(config)
+    end
+
+    class PicasaClient < Picasa::Client
+      include Base
+
+      def initialize(config)
         config = DEFAULT_CONFIG.merge(config)
         email = config['email']
         client_secrets_json = File.expand_path(config['client_secrets_json'])
         tokens_yml = File.expand_path(config['tokens_yml'])
-        
+
         raise ERR_EMAIL if email.nil?
         raise (ERR_CLIENT_SECRETS % client_secrets_json) unless File.file? client_secrets_json
         FileUtils.mkdir_p(File.dirname tokens_yml)
-        
+
         auth = GooglePhotos::Auth.new(email, client_secrets_json, tokens_yml)
-        Picasa::Client.new(user_id: email, access_token: auth.access_token)
+
+        super(user_id: email, access_token: auth.access_token)
+      end
+    end
+
+    class ExifReader
+      include Base
+
+      def initialize(config)
+        config = DEFAULT_CONFIG.merge(config)
+        @filename = config['exif_cache_yml']
+        begin
+          FileUtils.mkdir_p File.dirname(@filename)
+          @cache = YAML.load_file(@filename)
+          Jekyll.logger.debug 'GPhoto:', "Loaded EXIF cache from `#{@filename}`"
+        rescue => e
+          Jekyll.logger.debug 'GPhoto:', "Could not load EXIF cache from `#{@filename}` - cache will be empty"
+          @cache = {}
+        end
+
+        Jekyll::Hooks.register :site, :post_write do
+          save_cache
+        end
+      end
+
+      def for_url(url)
+        if @cache.include? url
+          Jekyll.logger.debug 'GPhoto:', "Using cached EXIF for image #{url}"
+          exif = @cache[url]
+        else
+          begin
+            exif = Exif::Data.new(open(url)).to_h
+            Jekyll.logger.debug 'GPhoto:', "Successfully fetched EXIF for image #{url}"
+            @cache[url] = exif
+          rescue => e
+            Jekyll.logger.warn 'GPhoto:', "Error fetching EXIF for image #{url}: #{e}"
+          end
+        end
+        exif
+      end
+
+      private
+      def save_cache
+        open(@filename, 'w') do |file|
+          file.write(YAML.dump(@cache))
+        end
+        Jekyll.logger.debug 'GPhoto:', "Saved EXIF cache to #{@filename}"
       end
     end
 
     class Generator < ::Jekyll::Generator
       include Base
-      
+
       PHOTO_REGEX = /\.(jpg|jpeg|gif|png)$/i
       VIDEO_REGEX = /\.(mp4|mov|mkv)$/i
 
       def generate(site)
+        config = site.config['gphoto']
+        picasa_client = PicasaClient.new(config)
+        exif_reader = ExifReader.new(config)
+
         groups = (site.pages + site.posts.docs).group_by {|doc| doc.data['gphoto_album']}.reject {|k, _| k.nil?}
         return if groups.empty?
-        client = get_client(site.config['gphoto'])
-        all_albums = client.album.list.entries
-        
+        all_albums = picasa_client.album.list.entries
+
         groups.each do |album_search_str, docs|
           album_id = find_album_id(all_albums, album_search_str)
           unless album_id
             Jekyll.logger.warn 'GPhoto:', "album matching `#{album_search_str}` not found"
             next
           end
-          album = client.album.show(album_id, imgmax: 'd', thumbsize: '400,800,1600')
+          album = picasa_client.album.show(album_id, imgmax: 'd', thumbsize: '400,800,1600')
           docs.each do |doc|
-            album_data = get_album_data(album)
+            album_data = get_album_data(album, exif_reader)
             cover_entry = album_data['entries'].find {|e| e['caption'].include? '#cover'}
 
             doc.data.deep_merge!({'gphoto_album_data' => album_data})
             doc.data['header'] = (doc.data['header'] || {}).dup
             doc.data.deep_merge!({'header' => {'overlay_image' => cover_entry['thumbnails'].last['url']}}) if cover_entry
           end
-        end          
+        end
       end
 
       private
@@ -123,11 +180,13 @@ EOS
         end
         album&.id
       end
-      
-      def get_album_data(album)
+
+      def get_album_data(album, exif_reader)
         {'entries' => album.entries.map {|entry|
            contents = Picasa::Utils.safe_retrieve(entry.parsed_body, 'media$group', 'media$content')
            best = contents.max_by{|i|i['width']}
+
+           exif_data = exif_reader.for_url(best['url']) if best['medium'] == 'image'
 
            exif = "#{entry.exif.make} #{entry.exif.model}"
            exif += " f/#{entry.exif.fstop}" if entry.exif.fstop
@@ -165,4 +224,3 @@ EOS
     end
   end
 end
-
